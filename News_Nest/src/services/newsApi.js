4030c1906215ea95aecd6e98b10bb5d8f5fd3ce8
Maheen Begum <maheen.begum0173@gmail.com>
2026-08-30 01:06:33 +0530

    commit

--- News_Nest/src/services/newsApi.js
+++ News_Nest/src/services/newsApi.js
@@ -5,25 +5,28 @@ const BASE_URL = "https://gnews.io/api/v4";
 
 // Top headlines
 export const getTopHeadlines = async () => {
-  const response = await axios.get(
-    `${BASE_URL}/top-headlines?country=in&lang=en&max=10&apikey=${API_KEY}`
-  );
+  const response = await axios.get(`${BASE_URL}/top-headlines`, {
+    params: {
+      country: "in",
+      lang: "en",
+      max: 10,
+      apikey: API_KEY,
+    },
+  });
 
   return response.data.articles;
 };
 
 // News by category
 export const getNewsByCategory = async (category) => {
-  const response = await axios.get(
-    `${BASE_URL}/top-headlines?topic=${category}&lang=en&max=10&apikey=${API_KEY}`
-  );
-
-  return response.data.articles;
-};
-export const searchNews = async (query) => {
-  const response = await axios.get(
-    `${BASE_URL}/search?q=${query}&lang=en&max=10&apikey=${API_KEY}`
-  );
+  const response = await axios.get(`${BASE_URL}/top-headlines`, {
+    params: {
+      topic: category,
+      lang: "en",
+      max: 10,
+      apikey: API_KEY,
+    },
+  });
 
   return response.data.articles;
 };
